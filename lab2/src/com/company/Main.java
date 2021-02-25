64bbe3d41fda28dcc30f2fc56475c6369e7cdbc7
GOvidiu20 <59805831+GOvidiu20@users.noreply.github.com>
2021-02-25 15:18:48 +0200

    Update Main.java

--- lab2/src/com/company/Main.java
+++ lab2/src/com/company/Main.java
@@ -3,21 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Source s1=new Source(sourceTypes.FACTORY,"Sursa1",10);
-        Source s2=new Source(sourceTypes.WAREHOUSE ,"Sursa2",35);
-        Source s3=new Source(sourceTypes.WAREHOUSE,"Sursa3",25);
-        Destination d1=new Destination("Destinatia1",20);
-        Destination d2=new Destination("Destinatia2",25);
-        Destination d3=new Destination("Destinatia3",25);
-        System.out.println(s1.toString());
-        System.out.println(s2.toString());
-        System.out.println(s3.toString());
-        System.out.println(d1.toString());
-        System.out.println(d2.toString());
-        System.out.println(d3.toString());
-        Problem p=new Problem(Source.numberSources,Destination.numberDestinations);
-        p.buildMatrix(Source.Sources,Destination.Destinations);
-        p.afisareMatrix();
-        p.rezolvare();
+        Source s1=new Source(sourceTypes.FACTORY,"Sursa1");
+        Source s2=new Source(sourceTypes.WAREHOUSE ,"Sursa2");
+        Source s3=new Source(sourceTypes.WAREHOUSE,"Sursa3");
+        Destination d1=new Destination("Destinatia1");
+        Destination d2=new Destination("Destinatia2");
+        Destination d3=new Destination("Destinatia3");
+
+        Source sources[]={s1,s2,s3};
+        Destination desti[]={d1,d2,d3};
+        int demands[]={20,25,25};
+        int supplys[]={10,35,25};
+        int cost[][]={{2,3,1},{5,4,8},{5,6,8}};
+
+        Problem problema=new Problem();
+        problema.setSources(sources);
+        problema.setDestinations(desti);
+        problema.setDemand(demands);
+        problema.setSupply(supplys);
+        problema.setCost(cost);
+        System.out.println(problema.toString());
+        problema.afisareCost();
     }
 }
