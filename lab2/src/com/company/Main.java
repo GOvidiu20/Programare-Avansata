23e2105605426d1cb8aa97e0b038e8a0d6ff8194
GOvidiu20 <59805831+GOvidiu20@users.noreply.github.com>
2021-02-28 20:32:05 +0200

    Update Main.java

--- lab2/src/com/company/Main.java
+++ lab2/src/com/company/Main.java
@@ -3,9 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Source s1=new Source(sourceTypes.FACTORY,"Sursa1");
-        Source s2=new Source(sourceTypes.WAREHOUSE ,"Sursa2");
-        Source s3=new Source(sourceTypes.WAREHOUSE,"Sursa3");
+
+        Warehouse s1=new Warehouse("Sursa1");
+        Warehouse s2=new Warehouse("Sursa2");
+        Warehouse s3=new Warehouse("Sursa3");
         Destination d1=new Destination("Destinatia1");
         Destination d2=new Destination("Destinatia2");
         Destination d3=new Destination("Destinatia3");
@@ -16,6 +17,7 @@ public class Main {
         int supplys[]={10,35,25};
         int cost[][]={{2,3,1},{5,4,8},{5,6,8}};
 
+        //Optional solve with my own method
         Problem problema=new Problem();
         problema.setSources(sources);
         problema.setDestinations(desti);
@@ -24,5 +26,18 @@ public class Main {
         problema.setCost(cost);
         System.out.println(problema.toString());
         problema.afisareCost();
+        System.out.println("costul este: " + problema.solution());
+
+        //Bonus-Solve with the Vogel's approximation method
+        int demands1[]={250,350,400,200};
+        int supplys1[]={300,400,500};
+        int cost1[][]={{3,1,7,4},{2,6,5,9},{8,3,3,2}};
+        Problem problema1=new Problem();
+        problema1.setDemand(demands1);
+        problema1.setSupply(supplys1);
+        problema1.setCost(cost1);
+        System.out.println(problema1.toString());
+        problema1.afisareCost();
+        System.out.println("costul este: " + problema1.VoguelSolution());
     }
 }
